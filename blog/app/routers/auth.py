627d13d03f36c7e32c9465a6497b2d96995b5bd6
tasddc1226 <tasddc@naver.com>
2022-09-05 15:29:08 +0900

    Feat: user login process

---
+++ blog/app/routers/auth.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from .. import schemas, models, oauth2
+from ..database import Base
+from ..utils.password import verify_password
+
+router = APIRouter(tags=["Auth"])
+
+
+@router.post("/login", response_model=schemas.Token)
+def login(login: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(Base.get_db)):
+    user = db.query(models.User).filter(models.User.email == login.username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="E-mail does not exist")
+
+    if not verify_password(login.password, user.password):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Incorrect password")
+
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
+
+    return {"access_token": access_token, "token_type": "bearer"}
