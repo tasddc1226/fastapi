2651f31c582186a383a3a3ce7b01ba2c0543a684
tasddc1226 <tasddc@naver.com>
2022-09-05 15:30:43 +0900

    Add: token response schema

--- blog/app/schemas.py
+++ blog/app/schemas.py
@@ -17,3 +17,12 @@ class UserResponse(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
