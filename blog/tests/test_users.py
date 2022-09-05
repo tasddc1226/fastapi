21a9fb05a6a10e644977b63a8edcfe9002297c6a
tasddc1226 <tasddc@naver.com>
2022-09-05 22:24:55 +0900

    Add: 유저 login test code 추가

--- blog/tests/test_users.py
+++ blog/tests/test_users.py
@@ -1,6 +1,9 @@
 import pytest
 from app import schemas
+from app.core.config import settings
 from tests.test_config import client, session, test_user
+
+from jose import jwt
 
 
 def test_새로운_유저_생성(client):
@@ -47,3 +50,31 @@ def test_존재하지_않는_유저_삭제(client):
     res = client.delete(f"/users/{not_exist_id}")
 
     assert res.status_code == 404
+
+
+def test_유저_로그인_성공(client, test_user):
+    user_data = {"username": test_user["email"], "password": test_user["password"]}
+    res = client.post("/login", data=user_data)
+    login_res = schemas.Token(**res.json())
+
+    payload = jwt.decode(login_res.access_token, settings.secret_key, settings.algorithm)
+    id = payload.get("user_id")
+
+    assert id == test_user["id"]
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
+
+
+@pytest.mark.parametrize(
+    "email, password, status",
+    [
+        ("wrong@email.com", "test", 403),
+        ("test@demo.com", "wrong-password", 403),
+        (None, "test", 422),
+        ("test@demo.com", None, 422),
+    ],
+)
+def test_유저_로그인_실패(client, test_user, email, password, status):
+    wrong_user_data = {"username": email, "password": password}
+    res = client.post("/login", data=wrong_user_data)
+    assert res.status_code == status
