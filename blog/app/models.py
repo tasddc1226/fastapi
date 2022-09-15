f8b3f4efe063afbea52b39d2eabe2c9a7ba7877a
tasddc1226 <tasddc@naver.com>
2022-09-15 15:10:07 +0900

    [Add] Like Model

--- blog/app/models.py
+++ blog/app/models.py
@@ -17,6 +17,13 @@ class Post(Base):
     user = relationship("User")
 
 
+class Like(Base):
+    __tablename__ = "likes"
+
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
+
+
 class User(Base):
     __tablename__ = "users"
 
